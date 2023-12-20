f9e1553bd1d9704b41dfd6caf731004f2c974de0
kofXlcz <cephas.dakwa@xcelsz.com>
2023-12-20 02:53:47 +0000

    setup app run concurrently

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
-const express = require("express");
+import express from "express";
 import mysql from "mysql2";
-const cors = require("cors");
-const path = require("path");
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -11,12 +15,13 @@ app.use(express.json());
 
 const PORT = 5000;
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "oish_db",
-});
+// create connection to database
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "Oish_db",
+// });
 
 app.listen(PORT, () => {
   console.log("app listening");
